5c5d47b0a00644194eb8584a448165a705336a29
Garret DePass <garretdepass@gmail.com>
2025-08-14 18:15:58 -0400

    Added new case study, and modal pattern for a smoother experience linking off the site.

--- src/components/sections/code.tsx
+++ src/components/sections/code.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { theme } from "../../styles/theme";
 import styled from "styled-components";
 import SectionWrapper from "../layout/section_wrapper";
-import Link from "../primitives/link";
+import ProjectModal from "../elements/project_modal";
 
 const ContentWrapper = styled.div`
   @media only screen and (max-width: ${theme.breakpoints.small}) {
@@ -27,12 +27,26 @@ const Text1 = styled.h1`
   text-align: left;
 `;
 
-const Text2 = styled.h2`
+const ProjectTitleContainer = styled.div`
+  display: flex;
+  align-content: baseline;
+  color: #569cd6;
+`;
+
+const ProjectTitle = styled.h2`
   font-size: ${theme.fontSizes.base};
   font-family: ${theme.fontFamily.code};
   text-align: left;
   margin-block-start: 0px;
   margin-block-end: 0px;
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+
+const Arrow = styled.span`
+  margin-top: 3px;
 `;
 
 const Text3 = styled.p`
@@ -69,56 +83,139 @@ const Thumbnail = styled.img`
   height: 120px;
 `;
 
+interface ProjectData {
+  id: number;
+  title: string;
+  company: string;
+  date: string;
+  skills: string;
+  description: string;
+  modalDescription: string;
+  thumbnailSrc: string;
+  projectImageSrc: string;
+  imageIsDark: boolean;
+  links: Array<{ label: string; url: string }>;
+}
+
+const projectsData: ProjectData[] = [
+  {
+    id: 0,
+    title: "Location and ID",
+    company: "Sana Benefits",
+    date: "2025",
+    skills: "Full-stack product design, design/eng pairing, service design",
+    description:
+      "Patient location and identity verification for an asynchronous, remote medical platform.",
+    modalDescription:
+      "Sana Benefits is an insurance company with an integrated medical practice providing asynchronous, remote, primary care for thousands of patients. Because Sana providers hold licenses in multiple states, and Sana members could be anywhere in the US, we needed a way to verify patient identity and location.",
+    thumbnailSrc: "/assets/images/code/location_identity_thumbnail.png",
+    projectImageSrc: "/assets/images/code/location_identity_thumbnail.png",
+    imageIsDark: false,
+    links: [
+      {
+        label: "Annotated Figma file",
+        url: "https://www.figma.com/design/8T9NA832SUheeNi2ho0In3/Location-and-Identity---2025?node-id=19-21243&t=J7TZMQixb4WwJaZL-11",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Dice Roller",
+    company: "Passion Project",
+    date: "2025",
+    skills: "React, MongoDB, Vitest, Netlify",
+    description:
+      "A minimal web app for tabletop roleplaying. It streamlines character and roll management so players can focus on the game.",
+    modalDescription:
+      "My wife and I are part of a remote tabletop roleplaying group that plays an older system called Deadlands. Over the last year my wife collected a patchwork of tools to use while playing: Google Sheets to manage stats, dice rollers not designed for the complex mechanics of the game, calculator apps, and Discord to track things like experience points and Fate Chips. We wanted a more cohesive solution, so I to build something better for our group’s specific needs.",
+    thumbnailSrc: "/assets/images/code/dice_roller_thumbnail.png",
+    projectImageSrc: "/assets/images/code/dice_roller_thumbnail.png",
+    imageIsDark: true,
+    links: [
+      {
+        label: "GitHub",
+        url: "https://github.com/garretdepass/deadlands-dice-roller",
+      },
+      {
+        label: "Live Demo",
+        url: "https://dice-roller-deadlands.netlify.app/",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "This Website",
+    company: "Portfolio",
+    date: "2025",
+    skills: "React, Typescript, GSAP, responsive design",
+    description: "A tool for the job hunt, and an example of my work.",
+    modalDescription:
+      "Design portfolios all start to feel similar after you've been looking at them for hours. This is a competitive job market, so I wanted to create something that stands out. No cookie-cutter templates, no boring case studies. My goal is to show what I'm capable of, while being transparent about my process and how the work looks under the hood.",
+    thumbnailSrc: "/assets/images/code/portfolio_thumbnail.png",
+    projectImageSrc: "/assets/images/code/portfolio_thumbnail.png",
+    imageIsDark: false,
+    links: [
+      {
+        label: "GitHub",
+        url: "https://github.com/garretdepass/deadlands-dice-roller",
+      },
+    ],
+  },
+  // ... more projects
+];
+
 const Code: React.FC = () => {
+  const [selectedProject, setSelectedProject] = useState<ProjectData | null>(
+    null
+  );
+
+  const openModal = (project: ProjectData) => {
+    setSelectedProject(project);
+  };
+
+  const closeModal = () => {
+    setSelectedProject(null);
+  };
+
   return (
     <SectionWrapper backgroundColor="#1f1f1f">
       <ContentWrapper>
-        {window.innerWidth < 824 ? (
-          <Text1>
-            // But we’re here to talk
-            <br />
-            // engineering. Check out
-            <br />
-            // some of my code!
-          </Text1>
-        ) : (
-          <Text1>
-            // But we’re here to talk engineering.
-            <br />
-            // Check out some of my code!
-          </Text1>
-        )}
+        <Text1>
+          // I'm happiest mixing code and design.
+          <br />
+          // Here's some of my past work.
+        </Text1>
 
         <CaseStudyContainer>
-          <CaseStudy>
-            <Thumbnail src={`${process.env.PUBLIC_URL}/assets/images/code/dice_roller_thumbnail.png`} />
-            <Link
-              $color="#569cd6"
-              newWindow={true}
-              href="https://github.com/garretdepass/deadlands-dice-roller"
-            >
-              <Text2>Dice Roller</Text2>
-            </Link>
-            <Text3>
-              A minimal web app for tabletop roleplaying. It streamlines
-              character and roll management so players can focus on the game.
-            </Text3>
-          </CaseStudy>
-          <CaseStudy>
-            <Thumbnail src={`${process.env.PUBLIC_URL}/assets/images/code/portfolio_thumbnail.png`} />
-            <Link
-              $color="#569cd6"
-              newWindow={true}
-              href="https://github.com/garretdepass/front-end-portfolio"
-            >
-              <Text2>This website</Text2>
-            </Link>
-            <Text3>
-              Want to see how I coded this website? The source code is available
-              on Github.
-            </Text3>
-          </CaseStudy>
+          {projectsData.map((project) => (
+            <CaseStudy key={project.id}>
+              {/* <Thumbnail
+                src={`${process.env.PUBLIC_URL}${project.thumbnailSrc}`}
+                onClick={() => openModal(project)} // Only thumbnail is clickable
+                style={{ cursor: "pointer" }}
+              /> */}
+              <ProjectTitleContainer>
+                <ProjectTitle onClick={() => openModal(project)}>
+                  {project.title}{" "}
+                </ProjectTitle>
+                <Arrow className="material-symbols-outlined">
+                  arrow_outward
+                </Arrow>
+              </ProjectTitleContainer>
+              {/* </Link> */}
+              <Text3>{project.description}</Text3>
+            </CaseStudy>
+          ))}
         </CaseStudyContainer>
+
+        {/* Single modal that shows different content */}
+        {selectedProject && (
+          <ProjectModal
+            project={selectedProject}
+            isOpen={!!selectedProject}
+            onClose={closeModal}
+          />
+        )}
       </ContentWrapper>
     </SectionWrapper>
   );
